a678bc7182e34a460f6f05be419749d0fba306a0
r0meroh <romeroh862@gmail.com>
2020-06-23 10:20:00 -0700

    Add project files.

---
+++ cppPractice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    //create an empty int
+    int getty;
+    //print out the address of the pointer
+    std::cout << "the address is: " << &getty << std::endl;
+    //assign the address to a different variable
+    int* addy = reinterpret_cast<int*>(&getty);
+    //print out the variables
+    std::cout << "the addy value is: " << *addy << std::endl;
+    std::cout << "the addy address is: " << &addy << std::endl;
+    //assigning a value to the newest pointer to change the value of the first object
+    *addy = 55;
+    //print out the original object
+    std::cout << "the new value is: " << getty << std::endl;
+    return 0;
+}
